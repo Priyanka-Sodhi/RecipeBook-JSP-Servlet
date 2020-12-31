be91a755a40a5cfcab8702b5b3a8f7b792b44f57
Priyanka-Sodhi <66174600+Priyanka-Sodhi@users.noreply.github.com>
2020-12-31 15:14:57 +0530

    Add files via upload

---
+++ WebContent/mycss/myjs.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Panda Eye move
+$(document).on( "mousemove", function( event ) {
+  var dw = $(document).width() / 15;
+  var dh = $(document).height() / 15;
+  var x = event.pageX/ dw;
+  var y = event.pageY/ dh;
+  $('.eye-ball1').css({
+    width : x,
+    height : y
+  });
+  $('.eye-ball2').css({
+    width : x,
+    height : y
+  });
+});
+
+// validation
+
+
+/*$('.btn').click(function(){
+  $('form').addClass('wrong-entry');
+    setTimeout(function(){ 
+       $('form').removeClass('wrong-entry');
+     },3000 );
+});*/
+
